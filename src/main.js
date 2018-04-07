b834678117d9ef927ba94367103695100c113db8
gbradthompson <gbradthompson@gmail.com>
2018-04-07 16:42:07 -0600

    update npm packages, add gametime and render test

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import '../share/gametime'
+import '../share/render'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
